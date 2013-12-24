5001136d6cc1c1aa1cc4ba8db033597111f4837e
Douglas Mak <github@douglasmak.com>
2013-12-23 23:18:04 -0800

    Implemented QuickSort in JavaScript with a random pivot

---
+++ Quick_Sort/quick_sort.js
@@ -0,0 +1,38 @@
+function swapElements(array, indexI, indexJ) {
+    var temp = array[indexI];
+    array[indexI] = array[indexJ];
+    array[indexJ] = temp;
+}
+
+function partition(arrayOfNumbers, first, last, pivotIndex) {
+    var pivotValue = arrayOfNumbers[pivotIndex];
+
+    // Move pivot element to the last spot
+    swapElements(arrayOfNumbers, pivotIndex, last);
+
+    var i = first; 
+
+    // One over from the right to exclude the pivot element
+    for(var j = first; j < last; j++ ) {
+        if(arrayOfNumbers[j] < pivotValue) {
+            swapElements(arrayOfNumbers, i, j);
+            i++;
+        }
+    }
+
+    // Move element to where it belongs
+    swapElements(arrayOfNumbers, i, last);
+    return i;
+}
+
+function QuickSortRandomPivot(arrayOfNumbers, first, last) {
+    if( first < last ) {
+        var pivotIndex = new Numbers(arrayOfNumbers.length).getRandomInt(first, last);
+        var newPivotIndex = partition(arrayOfNumbers, first, last, pivotIndex);
+        QuickSortRandomPivot(arrayOfNumbers, first, newPivotIndex-1);
+        QuickSortRandomPivot(arrayOfNumbers, newPivotIndex+1, last);
+    }
+    return arrayOfNumbers;
+}
+
+var sortedNumbers = QuickSortRandomPivot([10, 1, 9, 2, 8, 3, 7, 4, 6, 5], 0, numbersArray.length-1);   
