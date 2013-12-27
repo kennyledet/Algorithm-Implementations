b1fb9e0a764fc12d8218d2f0b06ce5f53905003e
Joseph Adams <whitegolem@gmail.com>
2013-12-27 09:07:42 +0100

    corrected the horspool's algorithm to now work correctly

--- Boyer_Moore_Horspool/javascript/bmh.js
+++ Boyer_Moore_Horspool/javascript/bmh.js
@@ -1,4 +1,4 @@
-// Boyer–Moore–Horspool algorithm (or Horspool's algorithm) is an algorithm 
+// Boyer–Moore–Horspool algorithm (or Horspool's algorithm) is an algorithm
 // for finding substrings in strings.
 
 function boyer_moore_horspool(haystack, needle) {
@@ -7,7 +7,7 @@ function boyer_moore_horspool(haystack, needle) {
         last = (needle.length - 1),
         offset = 0,
         scan;
-  
+
     // Generate the "bad match table", which is the location of offsets
     // to jump forward when a comparison fails
     Array.prototype.forEach.call(needle, function (char, i) {
@@ -16,8 +16,8 @@ function boyer_moore_horspool(haystack, needle) {
 
     // Now look for the needle
     while (offset <= maxOffset) {
-        // Search right-to-left, checking to see if the current offset at 
-        // needle and haystack match.  If they do, rewind 1, repeat, and if we 
+        // Search right-to-left, checking to see if the current offset at
+        // needle and haystack match.  If they do, rewind 1, repeat, and if we
         // eventually match the first character, return the offset.
         for (scan=last; needle[scan] === haystack[scan+offset]; scan--) {
             if (scan === 0) {
@@ -25,7 +25,7 @@ function boyer_moore_horspool(haystack, needle) {
             }
         }
 
-        offset += (badMatchTable[haystack[offset + last]] || last);
+        offset += (badMatchTable[haystack[offset + last]] || last+1);
     }
 
     return -1;
