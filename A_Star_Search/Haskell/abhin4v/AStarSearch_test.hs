f57288d9e34da05cb800b5e7710843c604beb84c
Abhinav Sarkar <abhinav.sarkar@gmail.com>
2013-12-29 13:44:27 +0530

    Switched to hash based collections, added quickcheck tests
    1. Moved to hash set and hash map from tree set and tree map because it does not make sense
       that the search nodes must be ordered which is a constraint imposed by tree set and tree map.
    2. Added quickcheck property based tests.

--- A_Star_Search/Haskell/abhin4v/AStarSearch_test.hs
+++ A_Star_Search/Haskell/abhin4v/AStarSearch_test.hs
@@ -1,15 +1,26 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module AStarSearchTest where
 
 import AStarSearch
+import Data.List (foldl')
 import Data.Maybe (fromJust)
+import Data.Sequence ((|>))
+import Test.QuickCheck
+import Test.QuickCheck.All
+import qualified Data.HashMap.Strict as Map
+import qualified Data.Sequence as Seq
 
 -- We use A* search to find the shortest path (path with least number of moves) of a knight
 -- from a start square to a goal square on a chess board.
 
+type Square = (Int, Int)
+type Path = [Square]
+
 -- Finds the next squares a knight can move to from a given square
 nextKnightPos (x, y) =
-	zip (filter isValidMove . map (\(dx, dy) -> (x + dx, y + dy)) $ moves) (repeat 1)
-	where
+  filter isValidMove . map (\(dx, dy) -> (x + dx, y + dy)) $ moves
+  where
     moves = [(1,2), (1,-2), (-1,2), (-1,-2), (2,1), (2,-1), (-2,1), (-2,-1)]
     isValidMove (x, y) = and [x > 0, x < 9, y > 0, y < 9]
 
@@ -18,23 +29,62 @@ nextKnightPos (x, y) =
 mkHeuristic (gx, gy) (x, y) = max (abs (x-gx)) (abs (y-gy)) `div` 2
 
 -- Finds the shortest path of the knight
-knightsShortestPath :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
+knightsShortestPath :: Square -> Square -> Path
 knightsShortestPath initSq goalSq =
-  snd . fromJust $ astarSearch initSq (== goalSq) nextKnightPos (mkHeuristic goalSq)
+  snd . fromJust
+  $ astarSearch initSq (== goalSq) (flip zip (repeat 1) . nextKnightPos) (mkHeuristic goalSq)
 
--- example runs
-main :: IO ()
-main = do
-  print $ knightsShortestPath (1,1) (2,2)
-  -- prints [(1,1),(3,2),(5,3),(4,1),(2,2)]
-  print $ knightsShortestPath (4,1) (7,2)
-  -- prints [(4,1),(5,3),(7,2)]
-  print $ knightsShortestPath (4,1) (8,7)
-  -- prints [(4,1),(6,2),(5,4),(7,5),(8,7)]
-  print $ knightsShortestPath (8,1) (7,8)
-  -- prints [(8,1),(7,3),(8,5),(6,6),(7,8)]
-  print $ knightsShortestPath (1,1) (8,8)
-  -- prints [(1,1),(2,3),(3,5),(4,3),(5,5),(7,6),(8,8)]
-  print $ knightsShortestPath (1,1) (1,1)
-  -- prints [(1,1)]
-  return ()
+-- Finds the shortest path using breadth first search. Used for checking if the path returned by
+-- A* search is indeed shortest.
+bfs :: Square -> Square -> Path
+bfs startSq goalSq =
+  bfs' goalSq (Map.singleton startSq noSuchSq) (Seq.singleton startSq)
+  where
+    noSuchSq = (-1,-1)
+
+    bfs' goalSq tracks open = let
+      (first, rest) = Seq.splitAt 1 open
+      currentSq = Seq.index first 0
+      in if currentSq == goalSq
+         then consPath currentSq
+         else let
+           nextSqs = filter (not . flip Map.member tracks) . nextKnightPos $ currentSq
+           tracks' = foldl' (\t s -> Map.insert s currentSq t) tracks nextSqs
+           in bfs' goalSq tracks' (foldl (|>) rest nextSqs)
+      where
+        consPath square =
+          if Map.member square tracks
+          then consPath (fromJust . Map.lookup square $ tracks) ++ [square]
+          else []
+
+-- Setup to generate arbitrary squares for testing
+newtype TestSquare = TestSquare { sq :: Square } deriving (Show)
+
+instance Arbitrary TestSquare where
+  arbitrary = do
+    x <- choose (1, 8)
+    y <- choose (1, 8)
+    return $ TestSquare (x, y)
+
+-- Properties to test
+prop_path_starts_with_start_square startSq goalSq =
+  head (knightsShortestPath (sq startSq) (sq goalSq)) == sq startSq
+
+prop_path_ends_with_goal_square startSq goalSq =
+  last (knightsShortestPath (sq startSq) (sq goalSq)) == sq goalSq
+
+prop_path_consists_of_valid_knights_moves startSq goalSq =
+  let path = knightsShortestPath (sq startSq) (sq goalSq)
+  in all isValidKnightsMove $ zip path (tail path)
+  where
+    isValidKnightsMove (sqFrom, sqTo) = sqTo `elem` nextKnightPos sqFrom
+
+prop_path_is_shortest startSq goalSq =
+  let path = knightsShortestPath (sq startSq) (sq goalSq)
+  in length path == length (bfs (sq startSq) (sq goalSq))
+
+-- Tests all the properties 1000 times each
+testAllProps = $forAllProperties $ quickCheckWithResult (stdArgs {maxSuccess = 1000})
+
+-- main function runs the tests. Type `runhaskell AStarSearch_test.hs` on command line to run the tests.
+main = testAllProps >> return ()
