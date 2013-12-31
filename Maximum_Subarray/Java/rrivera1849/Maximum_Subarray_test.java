cc8c0f7628c8f42e4f4b9fd532f560b89981ceae
Rafael A. Rivera Soto <rafaelriverasoto@gmail.com>
2013-12-30 20:50:37 -0400

    Added simple JUnit test

--- Maximum_Subarray/Java/rrivera1849/Maximum_Subarray_test.java
+++ Maximum_Subarray/Java/rrivera1849/Maximum_Subarray_test.java
@@ -1,26 +1,28 @@
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-public class Maximum_Subarray_test extends Maximum_Subarray {
-	public static void main (String [] args){
+
+import org.junit.Test;
+
+@RunWith(JUnit4.class)
+public class Maximum_Subarray_test extends Maximum_Subarray{
+
+	@Test
+	public void test() {
+		double[] testArray = {13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7};
+		String correctResult = "(7,10,43.0)";
+		String result = maxSubarray(testArray,0,testArray.length-1).toString();
 		
-		double testArray[] = {13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7};
-		double testArray2[] = {1,2,3,4,5,6,7};
+		String errorMessage = "Error, correct result is (7,10,43.0), instead got " + result;
 		
-		System.out.println("In the following array the maximum subarray is "
-				+ "bounded from index 7-10");
+		Assert.assertEquals(errorMessage,correctResult,result);
 		
-		for(double number: testArray){
-			System.out.print(number + " ");
-		}
+		double[] testArray2 = {1,2,3,4,5};
+		correctResult = "(0,4,15.0)";
+		result = maxSubarray(testArray2,0,testArray2.length-1).toString();
 		
-		System.out.println("\n\nOur algorithm calculates: " + maxSubarray(testArray,0,testArray.length-1));
-		
-		System.out.println("\nThe following array contains only positive numbers therefore "
-				+ "the maximum subarray is the complete array itself");
-		
-		for(double number: testArray2){
-			System.out.print(number + " ");
-		}
-		
-		System.out.println("\n\nOur algorithm calculates: " + maxSubarray(testArray2,0,testArray2.length-1));
+		Assert.assertEquals(errorMessage,correctResult,result);
 	}
+
 }
