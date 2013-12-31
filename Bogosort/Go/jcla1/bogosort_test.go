552d07c0aaaff01213030f248c78b45905fb84b0
Joseph Adams <whitegolem@gmail.com>
2013-12-31 16:43:29 +0100

    realized we don't need areSame, since we have tested isSorted

--- Bogosort/Go/jcla1/bogosort_test.go
+++ Bogosort/Go/jcla1/bogosort_test.go
@@ -18,24 +18,13 @@ func TestIsSorted(t *testing.T) {
 
 func TestBogosort(t *testing.T) {
 	arrs := [][]int{[]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}, []int{2, 3, 7, -5, -1, 4, -10}}
-	expectedResults := [][]int{[]int{-5, -3, -2, -1, 1, 1, 2, 4, 4}, []int{-10, -5, -1, 2, 3, 4, 7}}
 
 	var result []int
 
 	for i, expected := range expectedResults {
 		result = Bogosort(arrs[i])
-		if !areSame(result, expected) {
+		if !isSorted(result) {
 			t.Errorf("Bogosort(%v) = %v, want %v", arrs[i], result, expected)
 		}
 	}
-}
-
-func areSame(a, b []int) bool {
-    for i, v := range a {
-        if v != b[i] {
-            return false
-        }
-    }
-
-    return true
 }
