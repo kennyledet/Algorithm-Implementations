f0fb5f4b5f963385b7efbceee90cf0a9da0e8ece
Seamus Abshere <seamus@abshere.net>
2014-01-02 10:57:11 -0600

    save an array access

--- Insertion_Sort/Ruby/mitogh/insertion_sort.rb
+++ Insertion_Sort/Ruby/mitogh/insertion_sort.rb
@@ -1,8 +1,8 @@
 def insertion_sort(arr = [])    
   arr.each_with_index do |element, i| 
     j = i
-    while(j > 0 and arr[j-1] > element) do
-      arr[j] = arr[j-1]
+    while(j > 0 and (prev = arr[j-1]) > element) do
+      arr[j] = prev
       j -= 1
     end
     arr[j] = element
