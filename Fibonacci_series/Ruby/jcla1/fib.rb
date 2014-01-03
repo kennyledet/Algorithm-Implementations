ced49459f94556a9df0851e70bc60ef369dc4cc0
majjoha <mathias@mjj.io>
2014-01-03 19:33:18 +0100

    Fix errors in methods.

--- Fibonacci_series/Ruby/jcla1/fib.rb
+++ Fibonacci_series/Ruby/jcla1/fib.rb
@@ -12,23 +12,26 @@ def fib(n)
     else
         return fib(n-1) + fib(n-2)
     end
+end
 
 # This is the tail-call optimizabel version
 # of the fibonacci series.
 #
 # Note: Tail-call optimization is not
 # supported in all Ruby implementations.
-def fib_tco(n, a=0, b=1):
+def fib_tco(n, a=0, b=1)
     if n == 0
         a
     else
-        fib_tco_actual(n-1, b, a+b)
+        fib_tco(n-1, b, a+b)
+    end
+end
 
 # This implemenation does not rely on recursion,
 # which makes it optimal for large values of n,
 # since you can't cause a stack overflow.
 # The only limit you have is the maximum int value you can have.
-def fib_loop(n):
+def fib_loop(n)
     a, b = 0, 1
 
     while n != 0
@@ -38,3 +41,4 @@ def fib_loop(n):
     # No return neccessary, since Ruby
     # returns the last evaluated expression
     a
+end
