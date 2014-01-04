0550b6c8bb2b32928255608348a6dac7c0ef8723
Diego Allen <diegoallen@gmail.com>
2014-01-04 18:38:18 -0300

    Add test file for prim

--- Prim_Minimum_Spanning_Tree/Java/dalleng/PrimMST.java
+++ Prim_Minimum_Spanning_Tree/Java/dalleng/PrimMST.java
@@ -1,18 +1,19 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.PriorityQueue;
-import java.util.Scanner;
 import java.util.Set;
 
 public class PrimMST {
-    static Map<Integer, HashMap<Integer, Double>> adjacencyList = new HashMap<Integer, HashMap<Integer, Double>>();
-    static Set<Integer> mst = new HashSet<Integer>();
+    private Map<Integer, HashMap<Integer, Double>> adjacencyList = new HashMap<Integer, HashMap<Integer, Double>>();
+    private Set<Integer> mst = new HashSet<Integer>();
 
-    static void addEdge(int from, int to, double weight) {
+    public Set<Integer> getMST() {
+        return mst;
+    }
+
+    public void addEdge(int from, int to, double weight) {
         if (adjacencyList.containsKey(from)) {
             adjacencyList.get(from).put(to, weight);
         } else {
@@ -21,45 +22,46 @@ public class PrimMST {
             adjacencyList.put(from, edges);
         }
     }
-
-
-    static Set<Entry<Integer, Double>> neighborsAndWeight(int node) {
-        Set<Entry<Integer, Double>> neighborsAndWeight = new HashSet<Entry<Integer, Double>>();
-        if (adjacencyList.containsKey(node)) {
-            neighborsAndWeight = adjacencyList.get(node).entrySet();
-        }
-        return neighborsAndWeight;
-    }
-
-    static double prim(int start) {
-        HashSet<Integer> visited = new HashSet<Integer>();
+    
+    public double prim(int start) {
         Integer lastInserted = start;
         double sum = 0;
-        visited.add(start);
 
-        PriorityQueue<WeightedEdge> possibleEdges = new PriorityQueue<Main.WeightedEdge>();
+        PriorityQueue<WeightedEdge> possibleEdges = new PriorityQueue<WeightedEdge>();
+        HashSet<Integer> visited = new HashSet<Integer>();
+        visited.add(start);
 
         while (adjacencyList.size() != visited.size()) {
             for (Entry<Integer, Double> nw : neighborsAndWeight(lastInserted)) {
                 possibleEdges.add(new WeightedEdge(lastInserted, nw.getKey(), nw.getValue()));
             }
 
-            WeightedEdge edge = possibleEdges.poll();
+            if (possibleEdges.size() > 0) {
+                WeightedEdge edge = possibleEdges.poll();
 
-            while (visited.contains(edge.to)) {
-                edge = possibleEdges.poll();
+                while (visited.contains(edge.to)) {
+                    edge = possibleEdges.poll();
+                }
+
+                mst.add(edge.to);
+                visited.add(edge.to);
+                lastInserted = edge.to;
+                sum += edge.weight;
             }
-
-            mst.add(edge.to);
-            visited.add(edge.to);
-            lastInserted = edge.to;
-            sum += edge.weight;
         }
 
         return sum;
     }
 
-    static class WeightedEdge implements Comparable<WeightedEdge> {
+    private Set<Entry<Integer, Double>> neighborsAndWeight(int node) {
+            Set<Entry<Integer, Double>> neighborsAndWeight = new HashSet<Entry<Integer, Double>>();
+            if (adjacencyList.containsKey(node)) {
+                neighborsAndWeight = adjacencyList.get(node).entrySet();
+            }
+            return neighborsAndWeight;
+    }
+
+    private class WeightedEdge implements Comparable<WeightedEdge> {
         public int from;
         public int to;
         public double weight;
@@ -85,7 +87,6 @@ public class PrimMST {
 
             return ret;
         }
-
     }
 
 }
