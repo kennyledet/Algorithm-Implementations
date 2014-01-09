6217ace281c78e9a03ce11df47ecd7c05eb295b6
Joseph Adams <whitegolem@gmail.com>
2014-01-09 17:57:14 +0100

    go fmt 'ed all go files in repo

--- Bubble_Sort/Go/jcla1/bubblesort_test.go
+++ Bubble_Sort/Go/jcla1/bubblesort_test.go
@@ -3,24 +3,24 @@ package bubblesort
 import "testing"
 
 func TestBubbleSort(t *testing.T) {
-    arrs := [][]int{[]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}, []int{2, 3, 7, -5, -1, 4, -10}}
+	arrs := [][]int{[]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}, []int{2, 3, 7, -5, -1, 4, -10}}
 
-    var result []int
+	var result []int
 
-    for _, arr := range arrs {
-        result = BubbleSort(arr)
-        if !isSorted(result) {
-            t.Errorf("BubbleSort(%v) = %v", arr, result)
-        }
-    }
+	for _, arr := range arrs {
+		result = BubbleSort(arr)
+		if !isSorted(result) {
+			t.Errorf("BubbleSort(%v) = %v", arr, result)
+		}
+	}
 }
 
 func isSorted(arr []int) bool {
-    for i := 1; i < len(arr); i++ {
-        if arr[i-1] > arr[i] {
-            return false
-        }
-    }
+	for i := 1; i < len(arr); i++ {
+		if arr[i-1] > arr[i] {
+			return false
+		}
+	}
 
-    return true
+	return true
 }
