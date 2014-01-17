92bebf184e946ddde84056ac209eb319f9ec7d3e
Yonaba <roland.yonaba@gmail.com>
2014-01-17 11:32:26 +0000

    Moved metamethods to the class itself

--- A_Star_Search/Lua/Yonaba/node.lua
+++ A_Star_Search/Lua/Yonaba/node.lua
@@ -1,11 +1,17 @@
 -- A* Node class abstraction
 
+-- This class represents an abstraction of the A* Node.
+-- It should implemented with additional functions when
+-- writing a custom handler.
+
+-- See astar.lua comment header.
+-- See custom handlers for reference (*_hander.lua).
+
 local class = require 'class'
 
 local Node = class {g = 0, h = 0, f = 0}
-local Node_mt = getmetatable(Node)
-Node_mt.__eq = function(a, b) return a:isEqualTo(b) end
-Node_mt.__lt = function(a, b) return a.f < b.f end
-Node_mt.__tostring = function(n) return n:toString() end
+Node.__eq = function(a, b) return a:isEqualTo(b) end
+Node.__lt = function(a, b) return a.f < b.f end
+Node.__tostring = function(n) return n:toString() end
 
 return Node
