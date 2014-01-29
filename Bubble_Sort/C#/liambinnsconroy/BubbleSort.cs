4dbcb9a9d95ea27927c49b3bf3dc56511923e04d
liambinnsconroy <liam.binnsconroy@gmail.com>
2014-01-29 17:51:31 +1300

    C# implementation of BubbleSort

---
+++ Bubble_Sort/C#/liambinnsconroy/BubbleSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class BubbleSort
+    {
+        public static void Sort(int[] array)
+        {
+            int first, second, count = 0;
+
+            do
+            {
+                count = 0;
+
+                for (int i = 0; i < array.Count() - 1; i++)
+                {
+                    first = array[i];
+                    second = array[i + 1];
+
+                    if (first > second)
+                    {
+                        array[i] = second;
+                        array[i + 1] = first;
+                        count++;
+                    }
+                }
+            } while (count != 0);
+        }
+    }
+}
---
+++ Bubble_Sort/C#/liambinnsconroy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 3, 3, 4, 2, 7, 8 };
+           
+
+            foreach (int l in array)
+            {
+                Console.Write(l + " ");
+                
+            }
+
+            Console.WriteLine();
+
+            BubbleSort.Sort(array);
+
+            foreach (int l in array)
+            {
+                Console.Write(l + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
