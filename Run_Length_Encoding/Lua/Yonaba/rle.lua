0e3762ca8221246a6c4997ca3920b71385b5f75a
Yonaba <roland.yonaba@gmail.com>
2014-02-23 16:29:42 +0000

    Enhanced RLE implementation Added tests

--- Run_Length_Encoding/Lua/Yonaba/rle.lua
+++ Run_Length_Encoding/Lua/Yonaba/rle.lua
@@ -1,67 +1,37 @@
---Opens Input file, Retrieves its Size
+-- Run-Length Encoding Compression algorithm  implementation
+-- See: https://en.wikipedia.org/wiki/Run-length_encoding
 
-local file = io.open("test.txt","rb")
-local Ssize = file:seek("end")
-file:seek("set",0)
-
---RLEncodes a single string
-function encode(std,flag)
---Compresses data
-local rle = ''
-local flag = flag
-local i = 1
-repeat
-	local char = string.sub(std,i,i)
-	local stradd
-	local count=1
-		while string.sub(std,i+count,i+count) == char do
-		count=count+1
-		end
-		if count == 1 then
-			if char ~= flag then stradd = flag..char else stradd = string.rep(flag,2) end
-		elseif count > 1 and count <= 9 then stradd = count..flag..char
-		elseif count > 9 then
-			local length = count
-			stradd=''
-			while length > 9 do
-			stradd=stradd..'9'..flag..char
-			length = length-9
-			end
-			if length > 1 and length <=9 then stradd=stradd..length..flag..char
-			else
-				if char ~= flag then stradd = flag..char else stradd=string.rep(flag,2) end
-			end
-		end
-	rle=rle..stradd
-	i=i+count
-until (i>string.len(std))
-return rle
+-- Compresses an input string using RLE algorithm
+-- @str    :  an input string to be compressed
+-- returns : the encoded string
+function rle_encode(str)
+  local prev = str:sub(1,1)
+  local count = 0
+  local encoded = ''
+  for char in str:gmatch('.') do
+    if char == prev then
+      count = count + 1
+    else
+      encoded = encoded .. (count .. prev)
+      prev = char
+      count = 1
+    end
+  end
+  return encoded .. (count .. prev)
 end
 
-
-
-
-local comp = io.open("compresse.txt","wb")
-local rle = ''
-
---Iterates All over the input file lines for RLEncoding
-for line in file:lines() do
-local std = line
-rle = rle..encode(std,'@')..'\n'
+-- Decodes a given input
+-- @str    : an encoded string
+-- returns : the original string
+function rle_decode(str)
+  local decoded_str = ''
+  for count, match in str:gmatch('(%d+)([^%d]+)') do
+    decoded_str = decoded_str .. (match:rep(count))
+  end
+  return decoded_str
 end
 
---Closes input file
-file:close()
---Writing output file, Retrieves its sizes and closes it.
-comp:write(rle)
-local Esize=comp:seek("end")
-comp:close()
-
-
---Output RLE Encoding Results
---Run Lenght Encoding
-print('*************Run-Length-Encoding Program"*******************')
-print('Input File Size: '..Ssize..' bytes')
-print('Output File Size: '..Esize..' bytes')
-print('Compression Rate: '..string.format("%.2f",100*((Ssize-Esize)/Ssize))..'%')
-
+return {
+  encode = rle_encode,
+  decode = rle_decode,
+}
