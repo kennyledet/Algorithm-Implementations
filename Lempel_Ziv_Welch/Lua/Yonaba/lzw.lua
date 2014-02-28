d6563176e2b10999da4eb829eae1e67d32a01c81
Yonaba <roland.yonaba@gmail.com>
2014-02-28 22:06:10 +0000

    Enhanced LZW Lua implementation Added decoding function Added tests

--- Lempel_Ziv_Welch/Lua/Yonaba/lzw.lua
+++ Lempel_Ziv_Welch/Lua/Yonaba/lzw.lua
@@ -1,28 +1,70 @@
---[
-local dict = {}
-for i = 0, 255 do
-	--print(i,string.char(i))
-	dict[string.char(i)] = i
+-- Lempel-Ziv Welch compression data algorithm implementation
+-- See : http://en.wikipedia.org/wiki/LZW
+
+local function lzw_encode(str)
+  local w = ''
+  local result = {}
+  local dict_size = 255
+
+  -- Builds the dictionnary
+  local dict = {}
+  for i = 0, dict_size do
+    dict[string.char(i)] = i
+  end
+
+  local i = dict_size
+
+  for char in str:gmatch('.') do
+    -- Finds the longest string matching the input
+    local wc = w .. char
+    if dict[wc] then
+      -- Save the current match index
+      w = wc
+    else
+      -- Add the match to the dictionary
+      table.insert(result, dict[w])
+      i = i + 1
+      dict[wc] = i
+      w = char
+    end
+  end
+  if w~='' then
+    table.insert(result, dict[w])
+  end
+  return result
 end
 
-local function lzw(s)
-	local w = ''
-	local i = 255
-	for c in s:gmatch('.') do
-		if dict[w..c] then w = w..c
-		else
-			dict[w..c] = i + 1
-			w = c
-			i = i + 1
-		end
-	end
-	--print(w)
-	--table.foreach(dict, function(k,v)
-		--if v > 255 then print(k,v) end
-	--end)
-	return w
+local function lzw_decode(str)
+  local dict_size = 255
+
+  -- Builds the dictionary
+  local dict = {}
+  for i = 0, dict_size do
+    dict[i] = string.char(i)
+  end
+
+  local w = string.char(str[1])
+  local result = w
+  for i = 2, #str do
+    local k = str[i]
+    local entry = ''
+    if dict[k] then
+      entry = dict[k]
+    elseif k == dict_size then
+      entry = w .. w:sub(1,1)
+    else
+      return nil -- No match found, decoding error
+    end
+    result = result .. entry
+    dict_size = dict_size + 1
+    dict[dict_size] = w .. entry:sub(1,1)
+    w = entry
+  end
+  return result
 end
 
-print(lzw('TOBEORNOTTOBEORTOBEORNOT'))
---]]
+return {
+  encode = lzw_encode,
+  decode = lzw_decode,
+}
 
