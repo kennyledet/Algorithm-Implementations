fa81a55b48ad38cc15440c54ac50b47f36e1f73a
Yonaba <roland.yonaba@gmail.com>
2014-03-22 15:42:54 +0000

    Added 100 doors problem

---
+++ 100_Doors_Problem/Lua/Yonaba/100doors_test.lua
@@ -0,0 +1,29 @@
+-- Tests for 100doors.lua
+local hundred_doors = require '100doors'
+
+local total, pass = 0, 0
+
+local function dec(str, len)
+  return #str < len
+     and str .. (('.'):rep(len-#str))
+      or str:sub(1,len)
+end
+
+local function run(message, f)
+  total = total + 1
+  local ok, err = pcall(f)
+  if ok then pass = pass + 1 end
+  local status = ok and 'PASSED' or 'FAILED'
+  print(('%02d. %68s: %s'):format(total, dec(message,68), status))
+end
+
+run('Open doors are perfect squares of integers', function()
+  local open, closed = hundred_doors()
+  for i, v in ipairs(open) do
+    assert(math.floor(math.sqrt(v)) == math.sqrt(v))
+  end
+end)
+
+print(('-'):rep(80))
+print(('Total : %02d: Pass: %02d - Failed : %02d - Success: %.2f %%')
+  :format(total, pass, total-pass, (pass*100/total)))
