d80c031694a12944ee41fef3a5dd97d36f08688d
Yonaba <roland.yonaba@gmail.com>
2014-03-24 17:35:45 +0000

    Prettified SOR source

--- Successive_Over_Relaxation/Lua/Yonaba/sor.lua
+++ Successive_Over_Relaxation/Lua/Yonaba/sor.lua
@@ -4,26 +4,27 @@
 -- Creates a vector of values
 local function vector(len, v)
   local x = {}
-  for i = 1,len do x[i] = v end
+  for i = 1, len do x[i] = v end
   return x
 end
 
 -- Computes the norm of a given vector
 local function norm(v)
   local n = 0
-  for i,_v in ipairs(v) do
+  for i, _v in ipairs(v) do
     n = n + (_v * _v)
   end
   return math.sqrt(n)
 end
 
 -- Solves the given matrix using SOR algorithm
--- m       : a matrix
+-- m       : a matrix A in [A]*[x] = [b]
+-- b       : the result vector in [A]*[x] = [b]
 -- w       : the relaxation parameter, defaults to 1.86
 -- eps     : the convergence criterion, defaults to 1e-6
 -- maxiter : the maximum number of iterations, defaults to 1e4
 -- return  : a solution vector
-local function SOR(m, w, eps, maxiter)
+local function SOR(m, b, w, eps, maxiter)
   local n = #m
   local x = vector(n, 0)
   local q, p, sum
@@ -36,17 +37,17 @@ local function SOR(m, w, eps, maxiter)
   repeat
     t = t + 1
     q = norm(x)
-    for i=1,n do
-      sum = m[i][n+1]
-      for j=1,n do
-        if (i~=j) then
-          sum = sum - m[i][j]*x[j]
+    for i =1, n do
+      sum = 0
+      for j = 1, n do
+        if (i ~= j) then
+          sum = sum + m[i][j] * x[j]
         end
       end
-      x[i] = (1-w) * x[i] + w * sum/m[i][i]
+      x[i] = (1 - w) * x[i] + (w / m[i][i]) * (b[i] - sum)
     end
     p = norm(x)
-  until (math.abs(p-q) < eps) or (t >= maxiter)
+  until (math.abs(p - q) < eps) or (t >= maxiter)
 
   return x
 end
