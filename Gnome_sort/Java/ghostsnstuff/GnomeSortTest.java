6e93d5b610449bf5e30338a51d939f64f8152ccd
ghostsnstuff <jaredhalpert90@gmail.com>
2014-03-27 09:01:20 -0700

    Update GnomeSortTest.java

--- Gnome_sort/Java/ghostsnstuff/GnomeSortTest.java
+++ Gnome_sort/Java/ghostsnstuff/GnomeSortTest.java
@@ -2,12 +2,12 @@
 // 5 3 2 4 8 7 1  # pre-sort
 // 1 2 3 4 5 7 8  # post-sort 
 class GnomeSortTest extends GnomeSort {
-	public static void main(String[] args) {
-		int[] arr = {5,3,2,4,8,7,1};
-		log(arr);
-		System.out.println(" # pre-sort");
-		arr = GnomeSort(arr);
-		log(arr);
-		System.out.println(" # post-sort ");	
-	}
+  public static void main(String[] args) {
+    int[] arr = {5,3,2,4,8,7,1};
+    log(arr);
+    System.out.println(" # pre-sort");
+    arr = GnomeSort(arr);
+    log(arr);
+    System.out.println(" # post-sort ");	
+  }
 }
