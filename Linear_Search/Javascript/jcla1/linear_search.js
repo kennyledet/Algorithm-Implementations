4a34776dac28605b1c8b9f2dc01a4eead40ee34a
Joseph Adams <whitegolem@gmail.com>
2014-03-28 15:40:52 +0100

    Updated linear search in JS

--- Linear_Search/Javascript/jcla1/linear_search.js
+++ Linear_Search/Javascript/jcla1/linear_search.js
@@ -1,6 +1,6 @@
 function linear_search(arr, key) {
     for (var i = arr.length - 1; i >= 0; i--) {
-        if (arr[i] == key) return i;
+        if (arr[i] === key) return i;
     }
 
     return -1
