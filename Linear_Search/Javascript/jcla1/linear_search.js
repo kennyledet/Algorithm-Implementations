ef4bb94d1e188e6b6f6e8592a84fe833d360b49e
Joseph Adams <whitegolem@gmail.com>
2014-03-28 15:40:35 +0100

    Added another linear search in Javascript

--- Linear_Search/Javascript/jcla1/linear_search.js
+++ Linear_Search/Javascript/jcla1/linear_search.js
@@ -5,3 +5,12 @@ function linear_search(arr, key) {
 
     return -1
 }
+
+// Note: This implementation returns the index
+//       of the last element that equals the key.
+function linear_search_functional(arr, key) {
+    return arr.reduce(function(idx, val, new_index) {
+        if (val === key) return new_index;
+        return idx;
+    }, -1)
+}
