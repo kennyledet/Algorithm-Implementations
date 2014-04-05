37b95a1f2346c20bd84877ff134ff5462a635ffd
Yonaba <roland.yonaba@gmail.com>
2014-04-05 21:48:54 +0000

    Added Fibonacci method Renamed functions to Golden_Ratio_*

--- Golden_Ratio_Algorithms/Lua/Yonaba/golden_ratio.lua
+++ Golden_Ratio_Algorithms/Lua/Yonaba/golden_ratio.lua
@@ -10,7 +10,7 @@ local function fuzzy_equal(a, b, eps) return (math.abs(a - b) < eps) end
 -- seed    : (optional) seed to start the computation (defaults to 1)
 -- acc     : (optional) approximation accuracy (defaults to 1E-8)
 -- returns : an approximation of the Golden Ratio
-local function gold_number_root_iteration(seed, acc)
+local function golden_ration_root_iteration(seed, acc)
   local phi, phi2 = seed or 1
   acc = acc or 1e-8
   repeat
@@ -26,7 +26,7 @@ end
 -- seed    : (optional) seed to start the computation (defaults to 1)
 -- acc     : (optional) approximation accuracy (defaults to 1E-8)
 -- returns : an approximation of the Golden Ratio
-local function gold_number_fractional_iteration(seed, acc)
+local function golden_ration_fractional_iteration(seed, acc)
   local phi, phi2 = seed or 1
   acc = acc or 1e-8
   repeat
@@ -41,7 +41,7 @@ end
 -- seed    : (optional) seed to start the computation (defaults to 1)
 -- acc     : (optional) approximation accuracy (defaults to 1E-8)
 -- returns : an approximation of the Golden Ratio
-local function gold_number_babylonian_iteration(seed, acc)
+local function golden_ration_babylonian_iteration(seed, acc)
   local phi, phi2 = seed or 1
   acc = acc or 1e-8
   repeat
@@ -58,13 +58,35 @@ end
 -- acc     : (optional) approximation accuracy (defaults to 1E-8)
 -- returns : an approximation of the Golden Ratio
 local solve = require 'lib.newtonraphson'
-local function gold_number_equation(seed, acc)
+local function golden_ration_equation(seed, acc)
   return solve(function(x) return x * x - x - 1 end, seed, acc)
 end
 
+-- Computes an approximate value of the Golden Ratio
+-- Using Fibonacci series (implemented as an external dependency).
+-- See: http://en.wikipedia.org/wiki/Golden_ratio#Relationship_to_Fibonacci_sequence
+-- seed    : (optional) seed to start the computation (defaults to 2)
+-- acc     : (optional) approximation accuracy (defaults to 1E-8)
+-- returns : an approximation of the Golden Ratio
+local fib = (require 'lib.fib')
+local function golden_ration_fibonacci_iteration(seed, acc)
+  seed, acc = seed or 2, acc or 1e-8
+  assert(seed >= 2, 'order must be greater than 1')
+  local denum, num = fib(seed), fib(seed + 1)
+  local phi, phi2 = num / denum
+  repeat
+    phi2 = phi
+    seed = seed + 1    
+    denum, num = num, fib(seed)
+    phi = num / denum
+  until fuzzy_equal(phi, phi2, acc)
+  return phi
+end
+
 return {
-  root       = gold_number_root_iteration,
-  fractional = gold_number_fractional_iteration,
-  babylonian = gold_number_babylonian_iteration,
-  equation   = gold_number_equation,
+  root       = golden_ration_root_iteration,
+  fractional = golden_ration_fractional_iteration,
+  babylonian = golden_ration_babylonian_iteration,
+  equation   = golden_ration_equation,
+  fibonacci  = golden_ration_fibonacci_iteration
 }
