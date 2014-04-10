996b568f48457e260cff6b977d81408b648767d5
Joseph Adams <whitegolem@gmail.com>
2014-04-10 09:53:28 +0200

    Added a fractional iteration method for finding the golden ratio in JS.

--- Golden_Ratio_Algorithms/Javascript/jcla1/golden_ratio.js
+++ Golden_Ratio_Algorithms/Javascript/jcla1/golden_ratio.js
@@ -11,3 +11,15 @@ function golden_ratio_root_iteration(seed, acc) {
     return phi;
 }
 
+function golden_ration_fractional_iteration(seed, acc) {
+    var phi = seed || 1;
+    var acc = acc || 1E-8;
+    var tmp = 0;
+
+    while (!Math.abs(phi - tmp) <= acc) {
+        tmp = phi;
+        phi = 1 + (1 / phi)
+    }
+
+    return phi;
+}
