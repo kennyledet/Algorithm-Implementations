fba58b2c87204add1741afbebfbdc63ff5d1af4a
Joseph Adams <whitegolem@gmail.com>
2014-04-14 18:19:05 +0200

    Added fibonacci iteration method for finding Phi in JS.

--- Golden_Ratio_Algorithms/Javascript/jcla1/golden_ratio.js
+++ Golden_Ratio_Algorithms/Javascript/jcla1/golden_ratio.js
@@ -36,3 +36,32 @@ function golden_ratio_babylonian_iteration(seed, acc) {
 
     return phi;
 }
+
+// Warning: This is slow, probably because calculating
+//          the fibonacci terms is also slow.
+function golden_ration_fibonacci_iteration(seed, acc) {
+    var seed = seed || 2;
+    var acc = acc || 1E-8;
+
+    var denum = fibonacci(seed);
+    var num = fibonacci(seed + 1);
+
+    var phi = num / denum;
+    var tmp = 0;
+
+    while (!Math.abs(phi - tmp) <= acc) {
+        tmp = phi;
+        seed += 1;
+        denum = num;
+        num = fibonacci(seed);
+        phi = num / denum;
+    }
+
+    return phi;
+}
+
+// Gives the n-th fibonacci term
+function fibonacci(n) {
+    if (n <= 2) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2)
+}
