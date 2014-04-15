ce4ef9c45dfa6b3ddfb2b6e1dd0b2f8701a44ace
Joseph Adams <whitegolem@gmail.com>
2014-04-15 15:05:12 +0200

    Added Golden Ratio fractional iteration in Python

--- Golden_Ratio_Algorithms/Python/jcla1/golden_ratio.py
+++ Golden_Ratio_Algorithms/Python/jcla1/golden_ratio.py
@@ -1,8 +1,15 @@
 from math import sqrt
 
-def golden_ratio_root_iteration(phi=1, acc=1E-8):
-    tmp = 0
+def golden_ratio_root_iteration(phi=1.0, acc=1E-8):
+    tmp = 0.0
     while abs(phi - tmp) > acc:
         tmp = phi
         phi = sqrt(1 + phi)
+    return phi
+
+def golden_ratio_fractional_iteration(phi=1.0, acc=1E-8):
+    tmp = 0.0
+    while abs(phi - tmp) > acc:
+        tmp = phi
+        phi = 1 + (1 / phi)
     return phi
