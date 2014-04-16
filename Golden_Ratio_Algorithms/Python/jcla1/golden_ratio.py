04639b79daed3bc8c46ec03ffd26f5bba069af86
Joseph Adams <whitegolem@gmail.com>
2014-04-16 16:45:49 +0200

    Added Babylonian iteration method for finding Phi in Python.

--- Golden_Ratio_Algorithms/Python/jcla1/golden_ratio.py
+++ Golden_Ratio_Algorithms/Python/jcla1/golden_ratio.py
@@ -13,3 +13,10 @@ def golden_ratio_fractional_iteration(phi=1.0, acc=1E-8):
         tmp = phi
         phi = 1 + (1 / phi)
     return phi
+
+def golden_ratio_babylonian_iteration(phi=1.0, acc=1E-8):
+    tmp = 0
+    while abs(phi - tmp) > acc:
+        tmp = phi
+        phi = (phi * phi + 1) / (2 * phi - 1)
+    return phi
