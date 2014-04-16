54473ea1b5ccaefc56c04a57ecaca29bb5d91b05
Joseph Adams <whitegolem@gmail.com>
2014-04-16 16:50:39 +0200

    Added Fibonacci method for finding the Golden Ratio in Python.

--- Golden_Ratio_Algorithms/Python/jcla1/golden_ratio.py
+++ Golden_Ratio_Algorithms/Python/jcla1/golden_ratio.py
@@ -20,3 +20,22 @@ def golden_ratio_babylonian_iteration(phi=1.0, acc=1E-8):
         tmp = phi
         phi = (phi * phi + 1) / (2 * phi - 1)
     return phi
+
+def golden_ration_fibonacci_iteration(seed=2, acc=1E-8):
+    denum = fibonacci(seed)
+    num = fibonacci(seed + 1)
+
+    phi = num / denum
+    tmp = 0.0
+    while abs(phi - tmp) > acc:
+        tmp = phi
+        seed += 1
+        denum = num
+        num = fibonacci(seed)
+        phi = num / denum
+    return phi
+
+
+def fibonacci(n):
+    if n <= 2: return 1.0
+    return fibonacci(n - 1) + fibonacci(n - 2)
