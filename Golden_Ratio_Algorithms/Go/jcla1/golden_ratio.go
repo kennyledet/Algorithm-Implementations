3e55f51d85cbbb843c7255ade7b7d069a6c9a8b7
Joseph Adams <whitegolem@gmail.com>
2014-04-18 18:02:39 +0200

    Added fractional iteration method for finding phi in Go.

--- Golden_Ratio_Algorithms/Go/jcla1/golden_ratio.go
+++ Golden_Ratio_Algorithms/Go/jcla1/golden_ratio.go
@@ -12,3 +12,14 @@ func RootIteration(phi, epsilon float64) float64 {
 
 	return phi
 }
+
+func FractionalIteration(phi, epsilon float64) float64 {
+	tmp := 0.0
+
+	for math.Abs(tmp-phi) > epsilon {
+		tmp = phi
+		phi = 1 + (1 / phi)
+	}
+
+	return phi
+}
