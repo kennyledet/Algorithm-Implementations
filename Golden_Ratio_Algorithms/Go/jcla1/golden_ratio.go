4fca6edbebd3ff2150706be6ebced22e494c05b1
Joseph Adams <whitegolem@gmail.com>
2014-04-19 22:36:26 +0200

    Added fibonacci iteration method for finding phi in Go.

--- Golden_Ratio_Algorithms/Go/jcla1/golden_ratio.go
+++ Golden_Ratio_Algorithms/Go/jcla1/golden_ratio.go
@@ -34,3 +34,27 @@ func BabylonianIteration(phi, epsilon float64) float64 {
 
     return phi
 }
+
+// Tip: Use seed = 2 to start with!
+func FibonacciIteration(seed int, epsilon float64) float64 {
+    denum := fibonacci(seed)
+    num := fibonacci(seed + 1)
+
+    phi := num / denum
+    tmp := 0.0
+
+    for math.Abs(tmp-phi) > epsilon {
+        tmp = phi
+        seed += 1
+        denum = num
+        num = fibonacci(seed)
+        phi = num / denum
+    }
+
+    return phi
+}
+
+func fibonacci(n int) float64 {
+    if n <= 2 { return 1 }
+    return fibonacci(n - 1) + fibonacci(n - 2)
+}
