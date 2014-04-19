20cd151f38b660bff94908f5df5fcf6a8aadb0ec
Joseph Adams <whitegolem@gmail.com>
2014-04-19 22:30:51 +0200

    Added babylonian iteration method for finding phi in Go.

--- Golden_Ratio_Algorithms/Go/jcla1/golden_ratio.go
+++ Golden_Ratio_Algorithms/Go/jcla1/golden_ratio.go
@@ -23,3 +23,14 @@ func FractionalIteration(phi, epsilon float64) float64 {
 
 	return phi
 }
+
+func BabylonianIteration(phi, epsilon float64) float64 {
+    tmp := 0.0
+
+    for math.Abs(tmp-phi) > epsilon {
+        tmp = phi
+        phi = (phi * phi + 1) / (2 * phi - 1)
+    }
+
+    return phi
+}
