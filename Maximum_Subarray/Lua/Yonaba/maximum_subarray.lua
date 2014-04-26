fe6d8e2e89c1e037c1246b045e5cf9ad9543565d
Yonaba <roland.yonaba@gmail.com>
2014-04-26 09:12:02 +0000

    Added Brute-force algorithm

---
+++ Maximum_Subarray/Lua/Yonaba/maximum_subarray.lua
@@ -0,0 +1,54 @@
+-- Greatest subsequential sum finding algorithms implementation
+-- See: http://en.wikipedia.org/wiki/Maximum_subarray_problem
+
+-- Computes the largest sub-sequential sum using kadanes algorithm
+-- s       : a given sequence
+-- returns : the sum of the largest subsequence found
+-- returns : the starting index of the largest subsequence found
+-- returns : the final index of the largest subsequence found
+local function kadanes(s)
+  local n = #s
+  if n< 1 then return 0 end
+  local max_ending_here, max_so_far = s[1], s[1]
+  local begin, begin_tmp, last = 1, 1, 1
+  for i = 2, n do
+    if max_ending_here < 0 then
+      max_ending_here = s[i]
+      begin_tmp = i
+    else
+      max_ending_here = max_ending_here + s[i]
+    end
+    if max_ending_here >= max_so_far then
+      max_so_far = max_ending_here
+      begin = begin_tmp
+      last = i
+    end
+  end
+  return max_so_far, begin, last
+end
+
+-- Computes the largest sub-sequential sum using a brute-force algorithm
+-- s       : a given sequence
+-- returns : the sum of the largest subsequence found
+-- returns : the starting index of the largest subsequence found
+-- returns : the final index of the largest subsequence found
+local function gss_brute_force(s)
+  local st, ed = 1
+  local sum , max = s[st], s[st]
+  for i = 1, #s do
+    for j = i, #s do
+      sum = 0
+      for k = i, j do sum = sum + s[k] end
+      if sum > max then
+        st, ed = i, j
+        max = sum
+      end
+    end
+  end
+  return max, st, ed==nil and st or ed
+end
+
+return {
+  kadanes = kadanes,
+  brute_force = gss_brute_force,
+}
