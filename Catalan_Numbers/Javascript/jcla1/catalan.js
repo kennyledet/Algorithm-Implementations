839df1ba6532b0f81ee6e852ae59434e457a19b4
Joseph Adams <whitegolem@gmail.com>
2014-06-09 21:09:38 +0200

    Added binomial catalan number generator in JS

--- Catalan_Numbers/Javascript/jcla1/catalan.js
+++ Catalan_Numbers/Javascript/jcla1/catalan.js
@@ -2,6 +2,15 @@ function catalan_direct(n) {
     return fact(2 * n) / (fact(n + 1) * fact(n))
 }
 
+function catalan_binomial(n) {
+    if (n == 0) return 1;
+    return binomial_coeff(2 * n, n) - binomial_coeff(2 * n, n - 1)
+}
+
+function binomial_coeff(n, k) {
+    return fact(n) / (fact(n - k) * fact(k))
+}
+
 function fact (n) {
     if (n <= 1) return 1;
     return fact(n - 1) * n;
