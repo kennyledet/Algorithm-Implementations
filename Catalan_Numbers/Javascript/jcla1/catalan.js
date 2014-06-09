49999091ffd181957b81db94ad7be48deccf1b35
Joseph Adams <whitegolem@gmail.com>
2014-06-09 21:16:48 +0200

    Added catalan sum in JS

--- Catalan_Numbers/Javascript/jcla1/catalan.js
+++ Catalan_Numbers/Javascript/jcla1/catalan.js
@@ -7,6 +7,20 @@ function catalan_binomial(n) {
     return binomial_coeff(2 * n, n) - binomial_coeff(2 * n, n - 1)
 }
 
+function catalan_sum(n) {
+    return sum(range(0, n + 1).map(function(e) {
+        return Math.pow(binomial_coeff(n, e), 2)
+    })) / (n + 1);
+}
+
+function range(start, end) {
+    return Array.apply(null, Array(end-start)).map(function (_, i) {return start+i;});
+}
+
+function sum(arr) {
+    return arr.reduce(function(acc, e) { return acc + e; })
+}
+
 function binomial_coeff(n, k) {
     return fact(n) / (fact(n - k) * fact(k))
 }
