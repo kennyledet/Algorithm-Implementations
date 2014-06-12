3c63cadd845e175f4433b06db8ff11e0259cc752
Joseph Adams <whitegolem@gmail.com>
2014-06-12 22:05:54 +0200

    Added harmonic mean

--- Average/Javascript/jcla1/average.js
+++ Average/Javascript/jcla1/average.js
@@ -6,6 +6,12 @@ function geometric_mean(arr) {
     return Math.pow(prod(arr), 1 / arr.length);
 }
 
+function harmonic_mean(arr) {
+    return arr.length / sum(arr.map(function(n) {
+        return 1 / n;
+    }));
+}
+
 function sum(arr) {
     return arr.reduce(function(acc, n) {
         return acc + n;
