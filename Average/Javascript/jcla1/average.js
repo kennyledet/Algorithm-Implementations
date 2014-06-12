c1f5646df2bb278d8be3ec68813077d81cb8eb3f
Joseph Adams <whitegolem@gmail.com>
2014-06-12 22:04:53 +0200

    Added geometric mean in JS

--- Average/Javascript/jcla1/average.js
+++ Average/Javascript/jcla1/average.js
@@ -2,8 +2,18 @@ function arithmetic_mean(arr) {
     return sum(arr) / arr.length;
 }
 
+function geometric_mean(arr) {
+    return Math.pow(prod(arr), 1 / arr.length);
+}
+
 function sum(arr) {
     return arr.reduce(function(acc, n) {
         return acc + n;
+    });
+}
+
+function prod(arr) {
+    return arr.reduce(function(acc, n) {
+        return acc * n;
     });
 }
