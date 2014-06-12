d0f3a4e863c554d1779f4b1dcf47af6cf2c5c98e
Joseph Adams <whitegolem@gmail.com>
2014-06-12 22:10:39 +0200

    Added weighted mean

--- Average/Javascript/jcla1/average.js
+++ Average/Javascript/jcla1/average.js
@@ -18,6 +18,17 @@ function generalized_mean(arr, power) {
     })), 1 / power);
 }
 
+function weighted_mean(arr, weights) {
+    if (arr.length != weights.length) throw "# values & # weights have to be the same!";
+
+    var weighted = 0;
+    for (var i = arr.length - 1; i >= 0; i--) {
+        weighted += arr[i] * weights[i]
+    }
+
+    return weighted / sum(weights);
+}
+
 function sum(arr) {
     return arr.reduce(function(acc, n) {
         return acc + n;
