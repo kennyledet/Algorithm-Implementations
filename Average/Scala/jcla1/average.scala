d008fc991e251a8b205e6c7ffb3eaa1e9dadeee8
Joseph Adams <whitegolem@gmail.com>
2014-06-14 22:21:49 +0200

    Added harm. mean in Scala

--- Average/Scala/jcla1/average.scala
+++ Average/Scala/jcla1/average.scala
@@ -3,4 +3,7 @@ object Average {
 
     def geometricMean(xs: List[Double]): Double =
         math.pow(xs.product, 1.0 / xs.size)
+
+    def harmonicMean(xs: List[Double]): Double =
+        xs.size  / xs.map(n => 1.0 / n).sum
 }
