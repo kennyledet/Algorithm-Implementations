99578b0f1073de605155ed7cc834a48a59641fb1
Joseph Adams <whitegolem@gmail.com>
2014-06-14 22:21:23 +0200

    Added geom. mean in Scala

--- Average/Scala/jcla1/average.scala
+++ Average/Scala/jcla1/average.scala
@@ -1,3 +1,6 @@
 object Average {
     def arithmeticMean(xs: List[Double]): Double = xs.sum / xs.size
+
+    def geometricMean(xs: List[Double]): Double =
+        math.pow(xs.product, 1.0 / xs.size)
 }
