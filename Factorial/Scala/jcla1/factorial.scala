c3df06de3ac5879a3ea23a04d74ae388949ad6aa
Joseph Adams <whitegolem@gmail.com>
2014-06-17 16:51:31 +0200

    added range based factorial in scala

--- Factorial/Scala/jcla1/factorial.scala
+++ Factorial/Scala/jcla1/factorial.scala
@@ -1,5 +1,7 @@
 import scala.annotation.tailrec
 
+def factorial(n: Int): BigInt = (BigInt(1) to BigInt(n)).product
+
 def factorialTailRec(n: Int): BigInt = {
     @tailrec def fact(n: Int, acc: BigInt): BigInt = n match {
         case 0 | 1 => acc
