923920aef366f88215fcb887d79274c9dd2dc4a9
Joseph Adams <whitegolem@gmail.com>
2014-06-19 23:02:10 +0200

    Added proper type annotations

--- Maximum_Subarray/Haskell/jcla1/kadane.hs
+++ Maximum_Subarray/Haskell/jcla1/kadane.hs
@@ -1,11 +1,7 @@
---maxSubarray :: (Num a, Ord a) => [a] -> a
+maxSubarray :: (Num a, Ord a) => [a] -> a
 maxSubarray [x] = x
 maxSubarray (x:xs) = snd $ foldl msReducer (x, x) xs
 
+msReducer :: (Num a, Ord a) => (a, a) -> a -> (a, a)
 msReducer (maxEndingHere, maxSoFar) x = (meh, maxSoFar `max` meh)
     where meh = x `max` (maxEndingHere + x)
-
-
-
-
-main = putStrLn "Hello World"
