c6d5b695f853bfc06afd83509b98b162ccb8d30a
Joseph Adams <whitegolem@gmail.com>
2014-06-27 16:49:37 +0200

    Added default Pollard in Go

--- Pollard_Rho_Algorithm/Go/jcla1/pollard.go
+++ Pollard_Rho_Algorithm/Go/jcla1/pollard.go
@@ -4,6 +4,14 @@ import (
 	"math"
 )
 
+func DefaultPollard(n int) int {
+	return Pollard(n, defaultFunc)
+}
+
+func defaultFunc(x, n int) int {
+	return (x*x - 1) % n
+}
+
 func Pollard(n int, f func(int, int) int) int {
 	x, y, d := 2, 2, 1
 	for d == 1 {
