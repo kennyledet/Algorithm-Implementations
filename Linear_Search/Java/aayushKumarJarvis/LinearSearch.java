706cf58a1c824d5f192e17c4b54cdf56d86bcc4d
Jarvis@LNMIIT <iaayush.stevejobs@gmail.com>
2014-08-09 19:53:55 +0530

    Fixed Linear Search, Added a test

--- Linear_Search/Java/aayushKumarJarvis/LinearSearch.java
+++ Linear_Search/Java/aayushKumarJarvis/LinearSearch.java
@@ -4,7 +4,7 @@ public class LinearSearch {
 
     protected static int linearSearch(int arrayToBeSearched[], int numberToBeSearched) {
 
-        int valueFound = 0;
+        int valueFound = -1;
         for(int loopVariable=0;loopVariable<arrayToBeSearched.length;loopVariable++) {
 
             if(numberToBeSearched == arrayToBeSearched[loopVariable]) {
