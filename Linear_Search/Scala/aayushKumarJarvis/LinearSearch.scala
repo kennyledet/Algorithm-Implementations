4648682da3761f3bc457aaf21ba595ad367f3b07
Jarvis@LNMIIT <iaayush.stevejobs@gmail.com>
2014-08-12 14:45:45 +0530

    Adding Linear Search in Scala

---
+++ Linear_Search/Scala/aayushKumarJarvis/LinearSearch.scala
@@ -0,0 +1,12 @@
+
+class LinearSearchScala {
+
+  def linearSearch(listOfElements: List[Int], numberToSearch: Int) : Boolean = {
+
+    if(listOfElements.contains(numberToSearch))
+     true
+    else
+     false
+  }
+}
+
