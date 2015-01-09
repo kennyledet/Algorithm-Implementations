6b371824f9a63cba1bf31b26746400afdd046ed0
Ward Schodts <wardschodts@hotmail.com>
2015-01-09 19:37:17 +0100

    Added Prolog Implementation of Einstein Puzzle

---
+++ Einstein_Puzzle/Prolog/warreee/einstein_puzzle_test.pl
@@ -0,0 +1,9 @@
+% You can run this file with the command swipl -l einstein_puzzle_test.pl
+
+:- [einstein_puzzle].
+
+:- writeln('This is a version of the Einstein Problem with a possible solution:').
+
+:- main.
+
+:- halt.
