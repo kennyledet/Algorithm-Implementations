fab47ecbad3d629cc70c190aa77726845d5c4190
Davipb <daviparca@gmail.com>
2015-01-09 12:30:20 -0200

    Added C# implementation of Fibonacci

---
+++ Fibonacci_series/C#/Davipb/Fibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Fib
+{
+	public static class Fibonacci
+	{
+		/// <summary>
+		/// Generator method that calculates the Fibonacci sequence
+		/// </summary>
+		/// <returns>The fibonacci sequence</returns>
+		public static IEnumerable<ulong> Sequence()
+		{
+			ulong a = 1;
+			ulong b = 1;
+
+			yield return a;
+			yield return b;
+
+			while (true)
+			{
+				yield return a + b;
+
+				ulong temp = a;
+				a = b;
+				b = temp;
+				a += b;
+			}
+		}
+	}
+}
---
+++ Fibonacci_series/C#/Davipb/Fibonacci_test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fib
+{
+	public static class Fibonacci_test
+	{
+		static void Main(string[] args)
+		{
+			Test();
+			Console.ReadKey();
+		}
+
+		public static void Test()
+		{
+			foreach (var numb in Fibonacci.Sequence())
+			{
+				Console.WriteLine(numb);
+
+				if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+					break;
+			}
+		}
+	}
+}
