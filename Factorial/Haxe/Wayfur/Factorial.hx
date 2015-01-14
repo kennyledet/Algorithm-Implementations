97720e3bb9d7d259b958f131b8bc0d1c87b59ae9
Tyler <tylerjpond@gmail.com>
2015-01-13 21:50:30 -0330

    Implemented 64bit integer handling to Factorial

--- Factorial/Haxe/Wayfur/Factorial.hx
+++ Factorial/Haxe/Wayfur/Factorial.hx
@@ -1,20 +1,25 @@
 package ;
+import haxe.Int64;
 
 /**
  * Haxe implementation of Factorial
+ * 
+ * This implementation uses the Int64 class
+ * to provide handling of large numbers when
+ * haxe is compiled to specific platforms
  * 
  * @author Tyler Pond AKA Wayfur
  */
 class Factorial
 {
 
-	public function new(n:Int) 
-	{
-		trace(factorial(n));
-	}
+	public function new(){}
 	
-	private function factorial(n:Int):Float
+	public function factorial(n:Int):Int64
 	{ 
-		return if (n==1) 1 else n * factorial(n-1); 
+		var n64 = Int64.ofInt(n);
+		if (Int64.compare(n64, Int64.ofInt(1)) == 0)
+			return Int64.ofInt(1);
+		return Int64.mul(n64, factorial(Int64.toInt(n64) - 1)); 
 	} 
 }
