8720871afa51c7b0a1419a74c5d25dc8e28bfdf8
Patrick Yevsukov <pyevsukov@truveris.com>
2015-07-06 10:19:51 -0400

    Fix Typos

--- 99_Bottles_Problem/Python/tylerferraro/99_bottles.py
+++ 99_Bottles_Problem/Python/tylerferraro/99_bottles.py
@@ -3,7 +3,7 @@
 # Generate numbers 99 down to 1
 for bottles in (x for x in reversed(range(1, 100))):
   print("{0} {1} of beer on the wall, {0} {1} of beer.".format(bottles if bottles != 0 else "no more", "bottles" if bottles != 1 else "bottle"))
-  print("Take one down and pass it around, {0} {1} of beer on the wall.".format(bottles - 1 if bottles != 1 else "no more", "bottles" if bottles == 1 else "bottle"))
+  print("Take one down and pass it around, {0} {1} of beer on the wall.".format(bottles if bottles != 0 else "no more", "bottle" if bottles == 0 or bottles == 1 else "bottles"))
 
 print "No more bottles of beer on the wall, no more bottles of beer."
 print "Go to the store and buy some more, 99 bottles of beer on the wall."
