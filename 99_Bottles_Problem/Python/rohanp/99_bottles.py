35da61a86a8c7008a0b4c332e3277759856b3bf4
Rohan Pandit <rohanp@Rohans-MBP.myguests.gmu.edu>
2015-07-06 10:17:09 -0400

    fixed bug on output ln 198

--- 99_Bottles_Problem/Python/rohanp/99_bottles.py
+++ 99_Bottles_Problem/Python/rohanp/99_bottles.py
@@ -4,7 +4,7 @@ __author__ = "Rohan Pandit"
 
 for i in range(99, 0, -1):
     print("{0} {1} of beer on the wall, {0} {1} of beer.".format(i, "bottles" if i > 1 else "bottle"  ))
-    print("Take one down and pass it around, {0} {1} of beer on the wall".format(i - 1 if i > 1 else "no more", "bottles" if i - 1 > 1 else "bottle" ))
+    print("Take one down and pass it around, {0} {1} of beer on the wall".format(i - 1 if i > 1 else "no more", "bottles" if i - 1 != 1 else "bottle" ))
 
 print("No more bottles of beer on the wall, no more bottles of beer.")
 print("Go to the store and buy some more, 99 bottles of beer on the wall.")
